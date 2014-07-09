da1416c4631f33611d22cddff5ebff1d50f3bf53
wsheli <hl115781@163.com>
2014-07-09 18:20:37 +0800

    修改domain

--- src/main/java/com/eighth/airrent/domain/UserCollection.java
+++ src/main/java/com/eighth/airrent/domain/UserCollection.java
@@ -3,33 +3,52 @@ package com.eighth.airrent.domain;
 /**
  * Created by dam on 14-6-25.
  */
-public class UserCollection extends BaseDomain{
+public class UserCollection extends BaseDomain {
 
-    private String userId;
-    private String airportId;
-    private String planeId;
+	private String userId;
+	private String airportId;
+	private String planeId;
 
-    public String getUserId() {
-        return userId;
-    }
+	private String planeName;
+	private String airportName;
 
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
+	public void setAirportName(String airportName) {
+		this.airportName = airportName;
+	}
 
-    public String getAirportId() {
-        return airportId;
-    }
+	public String getAirportName() {
+		return airportName;
+	}
 
-    public void setAirportId(String airportId) {
-        this.airportId = airportId;
-    }
+	public String getPlaneName() {
+		return planeName;
+	}
 
-    public String getPlaneId() {
-        return planeId;
-    }
+	public void setPlaneName(String planeName) {
+		this.planeName = planeName;
+	}
 
-    public void setPlaneId(String planeId) {
-        this.planeId = planeId;
-    }
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getAirportId() {
+		return airportId;
+	}
+
+	public void setAirportId(String airportId) {
+		this.airportId = airportId;
+	}
+
+	public String getPlaneId() {
+		return planeId;
+	}
+
+	public void setPlaneId(String planeId) {
+		this.planeId = planeId;
+	}
 }
