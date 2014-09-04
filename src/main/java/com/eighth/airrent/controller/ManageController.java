5cd5740e6ea58e1da0687a13869084e1bff02f9c
longkai.zhao <zhaolongkai3k@gmail.com>
2014-09-05 00:27:45 +0800

    管理员界面

---
+++ src/main/java/com/eighth/airrent/controller/ManageController.java
@@ -0,0 +1,12 @@
+package com.eighth.airrent.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by kkk on 14/9/4.
+ */
+@Controller
+@RequestMapping(value = "/manage")
+public class ManageController {
+}
