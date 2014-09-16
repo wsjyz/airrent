3c8ddb3f5b52f08275e38bc013b2c4d8adb24621
wsheli <hl115781@163.com>
2014-09-16 21:59:39 +0800

    修改收藏接口

--- src/main/java/com/eighth/airrent/proxy/service/UserCollectionService.java
+++ src/main/java/com/eighth/airrent/proxy/service/UserCollectionService.java
@@ -11,21 +11,34 @@ import java.util.List;
  */
 public interface UserCollectionService {
 
-    /**
-     * 获取用户收藏的机场/飞机列表
-     * @param userId
-     * @param collectionType PLANE飞机 AIRPORT机场
-     * @return
-     */
-    @RemoteMethod(methodVarNames={ "userId","collectionType"})
-    List<UserCollection> findUserCollection(String userId,String collectionType)throws RemoteInvokeException;
+	/**
+	 * 获取用户收藏的机场/飞机列表
+	 * 
+	 * @param userId
+	 * @param collectionType
+	 *            PLANE飞机 AIRLINE机场
+	 * @return
+	 */
+	@RemoteMethod(methodVarNames = { "userId", "collectionType" })
+	List<UserCollection> findUserCollection(String userId, String collectionType)
+			throws RemoteInvokeException;
 
-    /**
-     * 收藏机场或者飞机
-     * @param collection
-     * @return SUCCESS成功，FAIL失败
-     * @throws RemoteInvokeException
-     */
-    @RemoteMethod(methodVarNames={ "collection"})
-    String addUserCollection(UserCollection collection)throws RemoteInvokeException;
+	/**
+	 * 收藏机场或者飞机
+	 * 
+	 * @param collection
+	 * @return SUCCESS成功，FAIL失败
+	 * @throws RemoteInvokeException
+	 */
+	@RemoteMethod(methodVarNames = { "collection" })
+	String addUserCollection(UserCollection collection)
+			throws RemoteInvokeException;
+
+	@RemoteMethod(methodVarNames = { "userId", "objId", "collectionType" })
+	boolean checkUserCollection(String userId, String objId,
+			String collectionType);
+
+	@RemoteMethod(methodVarNames = { "userId", "objId", "collectionType" })
+	boolean deleteUserCollection(String userId, String objId,
+			String collectionType);
 }
